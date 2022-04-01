11d4e7dfb96478ca03b5dea16aa94585a50862d0
aesadov <aesadov@gmail.com>
2022-04-02 02:18:47 +0300

    reduxForm added

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import {Field, InjectedFormProps, reduxForm} from 'redux-form';
+
+type FormDataType = {
+    login: string
+    password: string
+    rememberMe: boolean
+}
+
+const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
+    return <form onSubmit={props.handleSubmit}>
+        <div>
+            <Field component={'input'} name={'login'} placeholder={'Login'}/>
+        </div>
+        <div>
+            <Field component={'input'} name={'password'} placeholder={'Password'}/>
+        </div>
+        <div>
+            <Field component={'input'} name={'rememberMe'} type={'checkbox'}/> remember me
+        </div>
+        <div>
+            <button>Login</button>
+        </div>
+    </form>
+}
+
+const LoginReduxForm = reduxForm<FormDataType>({form: 'Login'})(LoginForm)
 
 const Login = () => {
-    return <h1>LOGIN</h1>
+    const onSubmit = (formData: FormDataType) => {
+      console.log(formData)
+    }
+    return <div>
+        <h1>Login</h1>
+        <LoginReduxForm onSubmit={onSubmit}/>
+    </div>
 }
 
 export default Login
--- src/components/redux/redux-store.ts
+++ src/components/redux/redux-store.ts
@@ -3,7 +3,8 @@ import profileReducer from './profile-reducer';
 import dialogsReducer from './dialogs-reducer';
 import usersReducer from './users-reducer';
 import authReducer from './auth-reducer';
-import thunkMiddleware from 'redux-thunk'
+import thunkMiddleware from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form'
 
 
 let rootReducer = combineReducers({
@@ -11,6 +12,7 @@ let rootReducer = combineReducers({
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
     auth: authReducer,
+    form: formReducer
 })
 
 let store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
